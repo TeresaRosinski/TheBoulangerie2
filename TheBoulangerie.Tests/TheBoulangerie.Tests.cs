ebcd7d41ee44d26ae061ad836623607c41d5812f
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-17 12:30:38 -0500

    Add successful instantiation test

--- TheBoulangerie.Tests/TheBoulangerie.Tests.cs
+++ TheBoulangerie.Tests/TheBoulangerie.Tests.cs
@@ -1,11 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TheBoulangerie;
+using System.Collections.Generic;
+using System; 
+using TheBoulangerie.Models;
 
 namespace TheBoulangerie.Tests
 {
   [TestClass]
   public class TheBoulangerieTests
   {
-    // Test methods go here
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread(); 
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
   }
 }
--- TheBoulangerie/Models/BreadClass.cs
+++ TheBoulangerie/Models/BreadClass.cs
@@ -2,6 +2,23 @@ namespace TheBoulangerie
 {
   public class BreadClass
   {
-    // properties, constructors, methods, etc. go here
+    public int BreadAmount = 0; 
+     private int CalculateBreadPrice() 
+    {
+      if (BreadAmount % 3 == 0)
+      {
+        return (BreadAmount*5)-(BreadAmount/3);
+      }
+      else if (BreadAmount%3 == 1)
+      {
+        return (BreadAmount*5)-(BreadAmount/3)+5;
+      }
+      else
+      {
+        return (BreadAmount*5)-(BreadAmount/3)+10;
+      }
     }
+
+    }
+ 
 }
