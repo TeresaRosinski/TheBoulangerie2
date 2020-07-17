9bb4a07ff9bddbc72999f827a65f04eb9983e15a
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-17 12:40:54 -0500

    adding failing BreadConstructor Quantity Test

--- TheBoulangerie.Tests/TheBoulangerie.Tests.cs
+++ TheBoulangerie.Tests/TheBoulangerie.Tests.cs
@@ -11,8 +11,18 @@ namespace TheBoulangerie.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread(); 
+      Bread newBread = new Bread(2); 
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
+    [TestMethod]
+    public void BreadConstructor_ReturnsQuantity_int()
+    {
+      int breadAmount = 5; 
+      Bread newBread = new Bread(breadAmount);
+      int result = newBread.BreadAmount; 
+      Assert.AreEqual(newBread, result);
+    }
+
   }
 }
