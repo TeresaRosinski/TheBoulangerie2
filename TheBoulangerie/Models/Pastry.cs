304aa6a8e01e647d03e9c590becf2f970eabd893
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-17 14:48:01 -0500

    add code for determining the price of pastries

--- TheBoulangerie/Models/Pastry.cs
+++ TheBoulangerie/Models/Pastry.cs
@@ -1,7 +1,30 @@
-namespace TheBoulangerie
+using System.Collections.Generic; 
+using System; 
+
+namespace TheBoulangerie.Models
 {
-  public class PastryClass
+  public class Pastry
   {
-    // properties, constructors, methods, etc. go here
-    }
+  public int PastryAmount {get;}
+
+    public Pastry (int pastryAmount)
+      {
+        PastryAmount = pastryAmount;
+      }
+     public int CalculatePastryPrice(int pastryAmount) 
+      { 
+        if (PastryAmount% 3 == 0)
+{
+return (PastryAmount*2)-(PastryAmount/3);
 }
+else if (PastryAmount% 3 == 1)
+{
+return ((PastryAmount*2)-((PastryAmount-1)/3));
+}
+else
+{
+return ((PastryAmount*2)-((PastryAmount-2)/3));
+}
+    }
+    }
+  }
